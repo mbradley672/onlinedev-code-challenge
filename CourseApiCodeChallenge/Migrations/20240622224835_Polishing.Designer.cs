4a425aff9aa333a9748acad455b8b7ddc2fe1483
Melvin Bradley <734084+mbradley672@users.noreply.github.com>
2024-06-22 18:49:13 -0400

    Refine data types and add non-clustered indexes
    The code adjustments mainly focused on refining the data types of certain fields like "Name" and "VideoUrl" in the "Sections", "Lessons", and "Courses" tables to have specific maximum lengths. Furthermore, non-clustered indexes were created for "Id" columns in "WatchLogs", "Sections", and "Lessons" tables, important for improving database performance during queries.

--- CourseApiCodeChallenge/Migrations/20240622224835_Polishing.Designer.cs
+++ CourseApiCodeChallenge/Migrations/20240622224835_Polishing.Designer.cs
@@ -4,6 +4,7 @@ using CourseApiCodeChallenge.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CourseApiCodeChallenge.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240622224835_Polishing")]
+    partial class Polishing
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CourseApiCodeChallenge/Migrations/20240622224835_Polishing.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CourseApiCodeChallenge.Migrations
+{
+    /// <inheritdoc />
+    public partial class Polishing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Sections",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "VideoUrl",
+                table: "Lessons",
+                type: "nvarchar(355)",
+                maxLength: 355,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Lessons",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Courses",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WatchLogs_Id",
+                table: "WatchLogs",
+                column: "Id")
+                .Annotation("SqlServer:Clustered", false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sections_Id",
+                table: "Sections",
+                column: "Id")
+                .Annotation("SqlServer:Clustered", false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lessons_Id",
+                table: "Lessons",
+                column: "Id")
+                .Annotation("SqlServer:Clustered", false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_WatchLogs_Id",
+                table: "WatchLogs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Sections_Id",
+                table: "Sections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Lessons_Id",
+                table: "Lessons");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Sections",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "VideoUrl",
+                table: "Lessons",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(355)",
+                oldMaxLength: 355);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Lessons",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Courses",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+        }
+    }
+}
